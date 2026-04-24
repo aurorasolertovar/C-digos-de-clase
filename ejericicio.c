ab70ecde0d16e0c494b7a338064b08b295b4f8aa
aurorasolertovar <aurora.fi1234@gmail.com>
2026-04-24 14:38:03 -0600

    Create ejericicio.c

---
+++ ejericicio.c
@@ -0,0 +1,39 @@
+
+    
+    printf("Imprimir Matriz\n");
+    i=0;
+    do
+    {
+        j=0;
+        
+        do 
+        {
+            printf("%d, ", matriz[i][j]);
+            j++;
+        }
+         while (j<2);
+        printf("\n");
+        i++;
+    }
+     while(i<2);
+     
+    int matriz2[2][2] = {{0,1},{1,0}};
+    int k, l;
+    printf("Imprimir Matriz\n");
+    k=0;
+    do
+    {
+        l=0;
+        
+        do 
+        {
+            printf("%d, ", matriz2[k][l]);
+            l++;
+        }
+         while (l<2);
+        printf("\n");
+        k++;
+    }
+     while(k<2);
+     
+     
